ed68528f28494a41376b87b970b13f457cf924af
Divya <dasdivya589@gmail.com>
2026-03-13 18:20:03 +0530

    added copy feature

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -6,6 +6,9 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
+
+  //useRef hook
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -25,6 +28,12 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setselectRange(0,20)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
@@ -40,12 +49,16 @@ function App() {
             value={password}
             className='outline-none w-full py-1 px-3'
             placeholder='password'
-            readOnly />
+            readOnly 
+            ref= {passwordRef}
+            />
 
           <button
+            onClick= {copyPasswordToClipboard}
             className='outline-none bg-blue-700 text-white
           px-3 py-0.5 shrink-0'
-          >copy</button>
+          >copy
+          </button>
         </div>
 
         <div className='flex text-sm gap-x-2'>
