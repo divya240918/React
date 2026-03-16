f148a5eac04f3483bd60610efff4cf61605e786f
Divya <dasdivya589@gmail.com>
2026-03-16 23:35:19 +0530

    error resolved

--- 06reactRouter/src/components/Home/home.jsx
+++ 06reactRouter/src/components/Home/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
@@ -32,12 +32,20 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img
+                        className="w-96"
+                        src="https://illustrations.popsy.co/amber/digital-nomad.svg"
+                        alt="hero illustration"
+                    />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img
+                    className="sm:w-96 w-48"
+                    src="https://illustrations.popsy.co/amber/work-from-home.svg"
+                    alt="remote work illustration"
+                />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
