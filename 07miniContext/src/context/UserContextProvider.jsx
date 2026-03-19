3c3a0f0d79c1538df9e6c29d54e028ebe2e0a916
Divya <dasdivya589@gmail.com>
2026-03-19 23:27:35 +0530

    send data

--- 07miniContext/src/context/UserContextProvider.jsx
+++ 07miniContext/src/context/UserContextProvider.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import UserContext from  from './userContext';
+import UserContext from  './userContext';
 
 const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
     return (
         <>
-            <UserContext.Provider>
+            <UserContext.Provider value={{user, setUser}}>
                 {children}
             </UserContext.Provider>
             
