acb0e2712a59d4f4d1564e5f7e096aa89655c039
Divya <dasdivya589@gmail.com>
2026-03-20 23:44:11 +0530

    final commit

--- 08themeswitcher/src/contexts/theme.js
+++ 08themeswitcher/src/contexts/theme.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 
-export const ThemeContext = createContext({
+export const ThemeContext = createContext({  //contextAPI: global storage
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {},
@@ -8,6 +8,6 @@ export const ThemeContext = createContext({
 
 export const ThemeProvider = ThemeContext.Provider
 //custom hooks
-export default function useTheme() {
+export default function useTheme() {  //custom hook: just to wrap useContext to make the code reusable
     return useContext(ThemeContext)
 }
