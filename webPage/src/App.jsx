650740ffec65f3eb1cb611e054ca062e26409684
Divya <dasdivya589@gmail.com>
2026-03-21 23:32:03 +0530

    Body content completed

--- webPage/src/App.jsx
+++ webPage/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Body from './components/Body'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Body />
       <Footer />
     </>
   )
---
+++ webPage/src/components/Body.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <>
+      <div className='m-2 bg-red-400 min-h-200'>
+        <div className='flex flex-wrap gap-10 p-10 justify-center'>
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/gelpen.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Gel Pen</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/case.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Pencil Case</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/diary.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Flower Diary</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+              <img 
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/handwash.png" alt="" />
+              <div className='text-white font-semibold flex gap-12 mb-2'>
+                <h4>Handwash</h4>
+                <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+              </div>
+            </div>            
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/marker.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Color Pen</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+              <img 
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/shampoo.png" alt="" />
+              <div className='text-white font-semibold flex gap-12 mb-2'>
+                <h4>Shampoo</h4>
+                <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+              </div>
+            </div> 
+            <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+              <img 
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/highlighter.png" alt="" />
+              <div className='text-white font-semibold flex gap-12 mb-2'>
+                <h4>Highlighter</h4>
+                <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+              </div>
+            </div>           
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/notebook.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Notebook</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>            
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/watch.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Smart Watch</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>
+          <div className='bg-blue-400 h-60 w-60 rounded-xl shadow-xl flex flex-col items-center justify-center'>
+            <img
+              className='h-40 m-5 rounded-lg shadow-md '
+              src="./public/tiffin.png" alt="" />
+            <div className='text-white font-semibold flex gap-12 mb-2'>
+              <h4>Tiffin Box</h4>
+              <button className='bg-red-400 w-10 rounded-md shadow-md text-sm'>Add</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Body
