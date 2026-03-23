d965ec67bfeaa94046093b4fdeb9ecccf0c12c04
Divya <dasdivya589@gmail.com>
2026-03-23 23:56:49 +0530

    add to cart funcaiotnality added

--- webPage/src/components/Login.jsx
+++ webPage/src/components/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
                     <div className='flex flex-col gap-10 items-center mt-6'>
                             <input type="text" className='rounded-lg p-1 w-[250px]' placeholder='username' />
                             <input type="password" className='rounded-lg p-1 w-[250px]' placeholder='password' />
-                            <button className=' bg-[#03045e] rounded-md text-white p-1 text-sm font-semibold w-[100px]'>Sign In</button>
+                            <button className=' text-[#03045e] rounded-md p-1 text-sm font-bold w-[100px]'>Sign In</button>
                     </div>
                 </div>
             </div>
---
+++ webPage/src/contexts/CartContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext(); //global storage is created
+
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product) => {
+        setCart((prev) => [...prev, product]);
+    };
+
+    <CartContext.Provider value={{cart, addToCart}}>
+        {children}
+    </CartContext.Provider>
+}
+
