6fd8b4d2053ba5bf266292158d68918c118fd72f
Divya <dasdivya589@gmail.com>
2026-03-27 19:30:08 +0530

    userSchema and model

---
+++ e-commerce/backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+const PORT = 3000
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req,res) => {
+    res.render("How much do you love me?");
+})
+
+app.listen(PORT, () => console.log(`App is listening at PORT ${PORT}`));
+
